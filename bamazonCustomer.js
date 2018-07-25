ed4a22924f8151089859aa24a4d7eba976ad3cce
mVang <meng.vang92@yahoo.com>
2018-07-25 17:12:33 -0500

    Customer View Complete

---
+++ bamazonCustomer.js
@@ -0,0 +1,95 @@
+var inquirer = require("inquirer")
+var mysql = require("mysql")
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "root",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Connection Successful!");
+    showItems();
+});
+
+function promptUser() {
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                name: "command",
+                message: "Which item do you want to purchase?",
+                type: "list",
+                choices: function () {
+                    var choices = [];
+                    for (var i = 0; i < results.length; i++) {
+                        choices.push(results[i].item_id + " | " + results[i].product_name)
+                    }
+                    return choices;
+                }
+            }
+        ]).then(function (answers) {
+            var splitItemId = answers.command.split(" ", 1);
+            var quantity = parseInt(splitItemId - 1);
+            // console.log(results[quantity].stock_quantity)
+            inquirer.prompt([{
+                name: "quantityPurchase",
+                type: "input",
+                message: "How many of these do you need?",
+                validate: function (value) {
+                    if (parseInt(value) < 0) {
+                        console.log("\nPlease enter a valid input.")
+                        return false;
+                    } else if ((isNaN(value) === false) && (results[quantity].stock_quantity >= parseInt(value))) {
+                        updateDb(results, quantity, value, splitItemId);
+                        return true;
+                    }
+                    console.log("\nQuantity specified not in supply. Please specify a different amount.")
+                    return false;
+                }
+            }]);
+        });
+    });
+}
+
+function showItems() {
+    connection.query("SELECT * FROM products", function (err, results) {
+        console.log("------------------------\n")
+        console.log("Items Available for Sale")
+        for (var i = 0; i < results.length; i++) {
+            console.log(
+                results[i].item_id +
+                " | " + results[i].product_name +
+                " | $" + results[i].consumer_price);
+        }
+        console.log("------------------------\n")
+        promptUser();
+    });
+}
+
+function updateDb(results, quantity, value, splitItemId) {
+    var query = connection.query(
+        "UPDATE products SET ? WHERE ?",
+        [
+            {
+                stock_quantity: results[quantity].stock_quantity - value
+            },
+            {
+                item_id: splitItemId
+            }
+        ],
+        function (err, res) {
+            console.log("\nDatabase Updated\n");
+            console.log("Thank you for your purchase!");
+        }
+    );
+}
